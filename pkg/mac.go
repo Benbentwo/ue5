a2c8de419d8a45f0cfad493c8a27cb855cb358c2
Benjamin Smith <ben.smith.developer@gmail.com>
2025-05-30 23:25:52 -0700

    fix mac and win

---
+++ pkg/mac.go
@@ -0,0 +1,9 @@
+//go:build darwin || linux
+// +build darwin linux
+
+package pkg
+
+// Stub function for UnrealEngineVersionSelectorExe on macOS or Linux.
+func UnrealEngineVersionSelectorExe() string {
+	return ""
+}
