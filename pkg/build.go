da9cf5ed2574e3d7a04818cf5c58c888f141aa68
Benjamin Smith <ben.smith.developer@gmail.com>
2026-01-04 20:58:18 -0800

    Add 'ue5 run' command to launch editor for current project

--- pkg/build.go
+++ pkg/build.go
@@ -9,6 +9,10 @@ import (
 var (
 	WindowsBuildScript = []string{"Engine", "Build", "BatchFiles", "Build.bat"}
 	UnixBuildScript    = []string{"Engine", "Build", "BatchFiles", "Mac", "Build.sh"}
+
+	WindowsEditorBinary = []string{"Engine", "Binaries", "Win64", "UnrealEditor.exe"}
+	MacEditorBinary     = []string{"Engine", "Binaries", "Mac", "UnrealEditor.app", "Contents", "MacOS", "UnrealEditor"}
+	LinuxEditorBinary   = []string{"Engine", "Binaries", "Linux", "UnrealEditor"}
 )
 
 func RunBuildScript(EnginePath, Target string, Platform string, State string, ProjectPath string) error {
@@ -23,4 +27,17 @@ func RunBuildScript(EnginePath, Target string, Platform string, State string, Pr
 	cmd := exec.Command(buildScript, Target, Platform, State, ProjectPath)
 
 	return RunCmd(cmd)
+}
+
+func RunEditor(EnginePath, ProjectPath string) error {
+	osPath := OsStringSliceSwitcher(WindowsEditorBinary, MacEditorBinary, LinuxEditorBinary)
+	basePath := []string{EnginePath}
+	pathElements := append(basePath, osPath...)
+	editorBinary := filepath.Join(pathElements...)
+
+	log.Info("Launching editor", "binary", editorBinary, "project", ProjectPath)
+
+	cmd := exec.Command(editorBinary, ProjectPath)
+	cmd.Start()
+	return nil
 }
